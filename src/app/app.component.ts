e64654d89b951c54ea23fc645e33d6cf15dcffab
Evans <ekioko404@gmail.com>
2024-05-08 13:36:14 +0300

    1st Push-----> Clean Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  template: `<h1>Hello Chereee Dev Team!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cheree';
+  title = 'homes';
 }
