d737f7d49d909197c83382005e15496422d1d1d5
Evans <ekioko404@gmail.com>
2024-05-09 13:03:15 +0300

    Navbar Works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello Chereee Dev Team!</h1>`,
+  template: `<section>
+    <app-navbar></app-navbar>
+  </section>`,
   styleUrls: ['./app.component.css'],
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'Cheree E-Commerce Web App';
