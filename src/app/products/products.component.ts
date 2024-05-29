cfd475fd2e4fb1f5abd5923871d392b3e7172164
sam-i-e <samierapo@outlook.com>
2024-05-29 10:22:34 +0300

    Products addition

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      products works!
+    </p>
+  `,
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+
+}
