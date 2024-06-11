06602f292b6fa8d0cdd42832c9c2b57312cc4ca5
Evans <ekioko404@gmail.com>
2024-06-11 23:43:12 +0300

    APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `<section>
+    <app-banner></app-banner>
+  </section>
+  <section>
     <app-navbar></app-navbar>
   </section>
   <section>
@@ -21,7 +25,7 @@ import { FooterComponent } from './footer/footer.component';
   </section>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [NavbarComponent, HeroComponent, ProductsComponent, FooterComponent]
+  imports: [NavbarComponent, HeroComponent, ProductsComponent, FooterComponent, BannerComponent]
 })
 export class AppComponent {
   title = 'Cheree E-Commerce Web App';
