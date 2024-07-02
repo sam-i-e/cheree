12a3a5d68159430e3d9d1a5f5be23c25dbd46625
sam-i-e <samierapo@outlook.com>
2024-07-02 15:27:12 +0300

    prices

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -11,27 +11,27 @@ import { Component } from '@angular/core';
     <div class="feature">
       <img src="assets/img7.jpg" alt="Untraceable Icon">
       <h2>Nike Air Max 97</h2>
-      <p>Sending and receiving addresses as well as transacted amounts.</p>
+      <p>Ksh 2500.</p>
     </div>
     <div class="feature">
       <img src="assets/img8.jpg" alt="Untraceable Icon">
       <h2>New Balance 990</h2>
-      <p>Sending and receiving addresses as well as transacted amounts.</p>
+      <p>Ksh 1700.</p>
     </div>
     <div class="feature">
       <img src="assets/img9.jpg" alt="Untraceable Icon">
       <h2>Vans Old Skool</h2>
-      <p>Sending and receiving addresses as well as transacted amounts.</p>
+      <p>Ksh 1499</p>
     </div>
     <div class="feature">
       <img src="assets/img10.jpg" alt="Autonomy Icon">
       <h2>Converse Chuck Taylor All Star</h2>
-      <p>FX has striven to solve blockchain governance, transparency brings.</p>
+      <p>Ksh 1999.</p>
     </div>
     <div class="feature">
       <img src="assets/img11.jpg" alt="Transparency Icon">
       <h2>Nike Air Force 1</h2>
-      <p>In Deriv, transparency brings trust & old strong transparency.</p>
+      <p>Ksh 2000</p>
     </div>
   </div>
 </div>
