2204680d4c7badf94d5c1845eee4060d9903796b
sam-i-e <samierapo@outlook.com>
2024-07-02 15:19:34 +0300

    Vans Old Skool

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -9,23 +9,23 @@ import { Component } from '@angular/core';
   <h1>The Largest & Best <span>Deriv Money Transfer</span> Service</h1>
   <div class="features">
     <div class="feature">
-      <img src="assets/img7.png" alt="Untraceable Icon">
+      <img src="assets/img7.jpg" alt="Untraceable Icon">
       <h2>Nike Air Max 97</h2>
       <p>Sending and receiving addresses as well as transacted amounts.</p>
     </div>
     <div class="feature">
-      <img src="assets/img8.png" alt="Untraceable Icon">
+      <img src="assets/img8.jpg" alt="Untraceable Icon">
       <h2>New Balance 990</h2>
       <p>Sending and receiving addresses as well as transacted amounts.</p>
     </div>
     <div class="feature">
-      <img src="assets/img9.png" alt="Untraceable Icon">
+      <img src="assets/img9.jpg" alt="Untraceable Icon">
       <h2>Vans Old Skool</h2>
       <p>Sending and receiving addresses as well as transacted amounts.</p>
     </div>
     <div class="feature">
-      <img src="assets/img10.png" alt="Autonomy Icon">
-      <h2>Puma Suede Classic</h2>
+      <img src="assets/img10.jpg" alt="Autonomy Icon">
+      <h2>Converse Chuck Taylor All Star</h2>
       <p>FX has striven to solve blockchain governance, transparency brings.</p>
     </div>
     <div class="feature">
