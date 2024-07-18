ec3ac64cae5ac8dbd9690dc41d7d27299ef41ea3
sam-i-e <samierapo@outlook.com>
2024-07-18 21:31:37 +0300

    hats component

--- src/app/hats/hats.component.ts
+++ src/app/hats/hats.component.ts
@@ -20,7 +20,7 @@ import { Component } from '@angular/core';
     </div>
     <div class="feature">
       <img src="assets/hat3.jpg" alt="Untraceable Icon">
-      <h2>Vintage-USA Trucker Hat</h2>
+      <h4>Vintage-USA Trucker Hat</h4>
       <p>Ksh 1499</p>
     </div>
     <div class="feature">
