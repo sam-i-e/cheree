d5b84c1f6b3f5ba37d97e0a4581fdfe8e99a9a25
sam-i-e <samierapo@outlook.com>
2024-07-23 14:42:06 +0300

    hoodies

--- src/app/hoodies/hoodies.component.ts
+++ src/app/hoodies/hoodies.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-hoodies',
   standalone: true,
   imports: [],
-  template: './hoodies.componen,
+  template: './hoodies.componen
   styleUrl: './hoodies.component.css'
 })
 export class HoodiesComponent {
