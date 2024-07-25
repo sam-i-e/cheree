b53b6516913db8fe4d3982e5eea007688ff6d88f
sam-i-e <samierapo@outlook.com>
2024-07-25 07:04:00 +0300

    blog edits

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -4,7 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-blog',
   standalone: true,
   imports: [],
-  template: '',
+  template: `
+  <div class="marketing-container">
+      <h2>Stay up to date on the latest news and SwiftCash Products</h2>
+      <div class="cards-container">
+        <div class="card" *ngFor="let item of items">
+          <img [src]="item.image" alt="News Image">
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
+          <button>More info</button>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
