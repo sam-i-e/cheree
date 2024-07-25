fc40469cee860579550fe59fde74f48adb377359
sam-i-e <samierapo@outlook.com>
2024-07-25 12:31:47 +0300

    blog edits

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
   <div class="marketing-container">
       <h2>Stay up to date on the latest news and Products</h2>
       <div class="cards-container">
-        <div class="card" *="let item of items">
+        <div class="card" *ngFor="let item of items">
           <img [src]="item.image" alt="News Image">
           <h3>{{ item.title }}</h3>
           <p>{{ item.description }}</p>
