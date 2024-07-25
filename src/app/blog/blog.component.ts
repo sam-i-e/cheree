e2d0d2f4053becf9c453448531643fda7183c6fe
sam-i-e <samierapo@outlook.com>
2024-07-25 13:38:42 +0300

    blog edits

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -29,18 +29,18 @@ export class BlogComponent {
     },
     {
       image: 'assets/img5.jpg',
-      title: 'SwiftCash Group Holdings Registers Strong Recovery',
-      description: 'Nairobi Monday 13th May 2024... After reporting a 5% decline in profit after tax for the...',
+      title: 'How to Find Hidden Gems in Thrift Stores',
+      description: 'Thrift shopping can be an adventure, and finding that perfect piece is a thrill like no other. At Cheree, we curate a collection of high-quality items to make your shopping experience enjoyable and rewarding. In this blog, we will share tips and tricks on how to spot those hidden gems and make the most out of your thrifting trips',
     },
     {
       image: 'assets/Herooo.jpg',
-      title: 'Sustainable Fashion: Why Thrifting is the Future',
-      description: 'As the fashion industry continues to grow, so does its impact on the environment. At Cheree, we believe in promoting sustainable fashion through thrifting. By purchasing pre-loved items, you are not only saving money but also reducing waste and conserving resources. Join us in making a positive impact on the planet, one stylish outfit at a time',
+      title: ' Behind the Scenes: How Cheree Curates Its Collection',
+      description: 'Ever wondered how we select the items that end up in our store? In this behind-the-scenes look, we take you through the curation process at Cheree. From sourcing high-quality pieces to ensuring each item meets our standards, learn how we bring you the best thrifted fashion online.',
     },
-    // {
-    //   image: 'path-to-image4.jpg',
-    //   title: 'EazzyBiz with SC',
-    //   description: 'EazzyBiz has all the tools designed to ensure that you can maintain your competitive advantage...',
-    // },
+    {
+      image: 'path-to-image4.jpg',
+      title: 'EazzyBiz with SC',
+      description: 'EazzyBiz has all the tools designed to ensure that you can maintain your competitive advantage...',
+    },
   ];
 }
